019b4be8118e413260f33d74ba681de2f0dc296f
KariukiAbel <abelmuriithi@gmail.com>
2022-02-05 11:25:13 +0300

    Successfully done the delete operation after getting a bug originating from a spelling mistake

--- base/views.py
+++ base/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Task
 
 # # Create your views here.
@@ -9,3 +13,27 @@ from .models import Task
     
 class TaskList(ListView):
     model = Task
+    context_object_name = 'tasks'
+    
+    
+class TaskDetail(DetailView):
+    model = Task
+    
+    
+class TaskCreate(CreateView):
+    model = Task
+    # fields = ['title', 'description', 'complete', 'created']
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    
+    
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    
+    
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
