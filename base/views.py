bd4f46a61f4a5e2c4587d007394762295b6f6ff5
KariukiAbel <abelmuriithi@gmail.com>
2022-02-07 16:57:12 +0300

    Just learned to use key word arguements **kwargs with context to filter data

--- base/views.py
+++ base/views.py
@@ -4,16 +4,32 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
 from .models import Task
 
 # # Create your views here.
 # def taskList(request):
 #     # return HttpResponse('To do List')
+
+class CustomLoginView(LoginView):
+    template_name = "base/login.html"
+    fields  = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
     
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ['tasks'] = context ['tasks'].filter(user = self.request.user)
+        context ['count'] = context ['tasks'].filter(complete = False).count()
+
+        return context
     
     
 class TaskDetail(DetailView):
