6da557e6ec2f4735a445436de3520879b4cfcecd
KariukiAbel <abelmuriithi@gmail.com>
2022-02-08 20:01:17 +0300

    Everything works fine. Finished setting up the search bar and its backend

--- base/views.py
+++ base/views.py
@@ -53,6 +53,13 @@ class TaskList(LoginRequiredMixin, ListView):
         # filter tasks by user
         context ['tasks'] = context ['tasks'].filter(user = self.request.user)
         context ['count'] = context ['tasks'].filter(complete = False).count()
+        
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains = search_input)
+            
+        context['search_input'] = search_input
+        
         return context
     
     
